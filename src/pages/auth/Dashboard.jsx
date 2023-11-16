c14d745be9f4ab66b4cb0b11658ce7583e25fa40
Juan Patrón <josejuanpatron1630@gmail.com>
2023-11-16 14:32:15 -0600

    protected routes added

---
+++ src/pages/auth/Dashboard.jsx
@@ -0,0 +1,3 @@
+export const Dashboard = () => {
+    return(<h1> Hola </h1>)
+}
--- src/routes/authRoutes.jsx
+++ src/routes/authRoutes.jsx
@@ -1,12 +1,26 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { LoginPage } from "../pages/auth/LoginPage";
+import { useAuth } from "../hooks/";
+import { authStates } from "../helpers";
+import { Dashboard } from "../pages/auth/Dashboard";
 
 export const AuthRoutes = () => {
+  const { auth } = useAuth();
+
   return (
     <>
       <Routes>
-        <Route path="/*" element={<Navigate to="/auth/login" />} />
-        <Route path="/auth/*" element={<LoginPage />} />
+        {auth == authStates.authenticated ? (
+          <>
+            <Route path="/home/*" element={<Dashboard />} />
+            <Route path="/*" element={ <Navigate to='/home' /> }/>
+          </>
+        ) : (
+          <>
+            <Route path="/*" element={<Navigate to="/auth/login" />} />
+            <Route path="/auth/*" element={<LoginPage />} />
+          </>
+        )}
       </Routes>
     </>
   );
